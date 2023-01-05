90c00f5d1ba9a17ed039ae54cf1e9bdb85bd3990
Wanchalermhonghom <wanchalerm.honghom@gmail.com>
2023-01-05 19:54:41 +0100

    login background

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,6 +4,9 @@ import LoginForm from "../components/LoginForm";
 const login = () => {
   return (
     <Flex className="h-screen bg-slate-100">
+      <img src="/moonjpg.jpeg" alt="" className="absolute  h-full w-full " />
+      <img src="/clouds.png" alt="" className="cloud-1 absolute" />
+      <img src="/clouds.png" alt="" className=" cloud-2 absolute" />
       <div className="m-auto">
         <LoginForm></LoginForm>
       </div>
